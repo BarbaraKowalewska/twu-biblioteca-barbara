f6661a68dc2a43423d8496cf8c85d24a04b83eec
Barbara Kowalewska <baw.kowalewska@gmail.com>
2020-01-28 10:50:13 +0100

    Adding desrciption to Menu for Returning Books

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -30,7 +30,7 @@ public class Menu {
 
     public void display() {
         System.out.println("Press number for the option you want");
-        System.out.println ( "1) List of books\n2) Quit\n3) Checkout the book" );
+        System.out.println ( "1) List of books\n2) Quit\n3) Checkout the book\n4) Return the book" );
         String userChoice = readLine();
         int choice = Integer.parseInt(userChoice);
             switch (choice) {
